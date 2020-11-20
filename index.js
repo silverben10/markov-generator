ee5db0d55ed845d90473a32deeb234aff6f9495a
Ben Silverman <bensilverman10@gmail.com>
2020-11-20 16:40:44 +0000

    Fix line endings

--- index.js
+++ index.js
@@ -4,44 +4,43 @@ class Markov {
    * Builds the generator
    * @param {object} props - The configuration options and input data
    */
-  constructor (props) {
-    this.props = props
+  constructor(props) {
+    this.props = props;
     if (!this.props.input) {
-      throw new Error('input was empty!')
+      throw new Error('input was empty!');
     }
-    this.terminals = {}
-    this.startWords = []
-    this.wordStats = {}
-    this.bannedTerminals = (props.bannedTerminals && props.bannedTerminals.length) ? props.bannedTerminals.map(word => word.toLowerCase()) : [];
+    this.terminals = {};
+    this.startWords = [];
+    this.wordStats = {};
+    this.bannedTerminals = (props.bannedTerminals && props.bannedTerminals.length) ? props.bannedTerminals.map((word) => word.toLowerCase()) : [];
 
     this.props.input.forEach((e, i, a) => {
-      let words = e.split(' ')
-      let lastWord = words[words.length - 1]
-      let firstWord = words[0]
+      const words = e.split(' ');
+      const lastWord = words[words.length - 1];
+      const firstWord = words[0];
 
       // if this.terminals contains the last word in this sentence, add to it's counter
       // otherwise, create the property on this.terminals and set it to 1
       if (this.terminals[lastWord]) {
-        this.terminals[lastWord]++
+        this.terminals[lastWord]++;
       } else {
-        this.terminals[lastWord] = 1
+        this.terminals[lastWord] = 1;
       }
-
 
       // this function tests to see if this.startWords already contains the first word or not
       // we can't use Array.prototype.includes, because when comparing each element in this.startWords to the first word, we need to compare them as lowercase
-      let checkWordNotInStartWords = (refWord) =>{
+      const checkWordNotInStartWords = (refWord) => {
         this.startWords.forEach((elem, ind, arr) => {
           if (elem.toLowerCase() === firstWord.toLowerCase()) {
-            return false
+            return false;
           }
-        })
-        return true
-      }
+        });
+        return true;
+      };
 
       // if the first word is not a space, and if this.startWords does not already contain the first word, add it
       if (firstWord.length && checkWordNotInStartWords(firstWord)) {
-        this.startWords.push(firstWord)
+        this.startWords.push(firstWord);
       }
 
       // loop through each word in current sentence
@@ -52,19 +51,19 @@ class Markov {
         // we store all of the keys in this.wordStats as lowercase to make the function makeChain case insensitive
         if (ar[it + 1]) {
           if (this.wordStats.hasOwnProperty(el.toLowerCase())) {
-            this.wordStats[el.toLowerCase()].push(ar[it + 1])
+            this.wordStats[el.toLowerCase()].push(ar[it + 1]);
           } else {
-            this.wordStats[el.toLowerCase()] = [ar[it + 1]]
+            this.wordStats[el.toLowerCase()] = [ar[it + 1]];
           }
         }
-      })
-    })
+      });
+    });
 
-    for (let word in this.terminals) {
-      if (this.terminals[word] === '' || !this.terminals[word]) { delete this.terminals[word] }
+    for (const word in this.terminals) {
+      if (this.terminals[word] === '' || !this.terminals[word]) { delete this.terminals[word]; }
     }
-    delete this.terminals['']
-    delete this.wordStats['']
+    delete this.terminals[''];
+    delete this.wordStats[''];
   }
 
   isBannedTerminal(word) {
@@ -76,7 +75,7 @@ class Markov {
    * @param {array} a - An array to randomly choose an element from
    * @return {string} The selected element of the array
    */
-  choice (a) {
+  choice(a) {
     return a[Math.floor(a.length * Math.random())];
   }
 
@@ -85,12 +84,12 @@ class Markov {
    * @param {number} minLength - The minimum number of words in the generated string
    * @return {string} The generated string
    */
-  makeChain (minLength = this.props.minLength || 10) {
+  makeChain(minLength = this.props.minLength || 10) {
     let word = this.choice(this.startWords);
-    let chain = [word];
+    const chain = [word];
 
     while (this.wordStats.hasOwnProperty(word.toLowerCase())) {
-      let nextWords = this.wordStats[word.toLowerCase()];
+      const nextWords = this.wordStats[word.toLowerCase()];
       word = this.choice(nextWords);
       chain.push(word);
       if (chain.length > minLength && (this.terminals.hasOwnProperty(word) && !this.isBannedTerminal(word))) {
@@ -105,7 +104,6 @@ class Markov {
     }
     return chain.join(' ');
   }
-
 }
 
-module.exports = Markov
+module.exports = Markov;
--- tests/test.js
+++ tests/test.js
@@ -1,56 +1,54 @@
-let expect = require('chai').expect
-let MarkovGen = require('../index.js')
+const { expect } = require('chai');
+const MarkovGen = require('../index.js');
 
 describe('Markov Chain Generator', () => {
-  let testInput = ['Test sentence number 1', 'This is another test sentence', 'I\'m test sentence number two!', 'I can\'t believe that this is another test', 'TEST of insensitive']
+  const testInput = ['Test sentence number 1', 'This is another test sentence', 'I\'m test sentence number two!', 'I can\'t believe that this is another test', 'TEST of insensitive'];
 
   it('throws with no input', () => {
-    let fn = () => {
-      return new MarkovGen({})
-    }
-    expect(fn).to.throw(Error)
-  })
+    const fn = () => new MarkovGen({});
+    expect(fn).to.throw(Error);
+  });
 
   it('returns a string', () => {
-    let string = new MarkovGen({
+    const string = new MarkovGen({
       input: testInput,
-      minLength: 2
-    }).makeChain()
-    expect(string).to.not.equal('')
-  })
+      minLength: 2,
+    }).makeChain();
+    expect(string).to.not.equal('');
+  });
 
   it('returns a string with a minimum amount of words set to the configuration variable', () => {
-    let minlenstr = new MarkovGen({
+    const minlenstr = new MarkovGen({
       input: testInput,
-      minLength: 3
-    }).makeChain()
-    let minlenarr = minlenstr.split(' ')
-    expect(minlenarr.length).to.be.at.least(3)
-  })
+      minLength: 3,
+    }).makeChain();
+    const minlenarr = minlenstr.split(' ');
+    expect(minlenarr.length).to.be.at.least(3);
+  });
 
   it('returns a string with a minimum amount of words set to a default value', () => {
-    let minlenstr = new MarkovGen({
+    const minlenstr = new MarkovGen({
       input: testInput,
-    }).makeChain()
-    let minlenarr = minlenstr.split(' ')
-    expect(minlenarr.length).to.be.at.least(10)
-  })
+    }).makeChain();
+    const minlenarr = minlenstr.split(' ');
+    expect(minlenarr.length).to.be.at.least(10);
+  });
 
   it('returns a string with a minimum amount of words set to an override value', () => {
-    let minlenstr = new MarkovGen({
+    const minlenstr = new MarkovGen({
       input: testInput,
-      minLength: 1
-    }).makeChain(5)
-    let minlenarr = minlenstr.split(' ')
-    expect(minlenarr.length).to.be.at.least(5)
-  })
+      minLength: 1,
+    }).makeChain(5);
+    const minlenarr = minlenstr.split(' ');
+    expect(minlenarr.length).to.be.at.least(5);
+  });
 
   it('returns a string with proper letter casing', () => {
-    let testInput = ['Test seNtence', 'anothEr teSt sentenCe', 'A thIRd tEST seNtence']
-    let string = new MarkovGen({
+    const testInput = ['Test seNtence', 'anothEr teSt sentenCe', 'A thIRd tEST seNtence'];
+    const string = new MarkovGen({
       input: testInput,
-      minLength: 2
-    }).makeChain()
-    expect(string).to.not.equal(string.toLowerCase())
-  })
-})
+      minLength: 2,
+    }).makeChain();
+    expect(string).to.not.equal(string.toLowerCase());
+  });
+});
